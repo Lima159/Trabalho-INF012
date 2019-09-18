f53ac7ad42a019a7262223fd4d6f695dd087ae5f
unknown <pfonseca485@gmail.com>
2019-09-17 21:33:12 -0300

    Intregado projeto laravel

---
+++ api/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use GuzzleHttp\Client;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('json-api', 'ApiController@index');
+Route::get('/json-api', function() {
+	$client = new Client();
+
+	$response = $client->request('GET', 'https://google.com/');
+	$statusCode = $response->getStatusCode();
+	$body = $response->getBody()->getContents();
+
+	return $body;
+});
