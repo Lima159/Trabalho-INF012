292748d7ec28eddc201bf0077b46067cde6b0e63
lima159 <igoralwork@gmail.com>
2019-09-18 17:45:58 -0300

    Tentando implementar auth token

--- django/biblioteca/models.py
+++ django/biblioteca/models.py
@@ -3,16 +3,26 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+#problema na geração automatica esta por aqui
 class CustomUser(AbstractUser):
     user_registration = models.CharField(max_length=30, primary_key=True) #matricula
     adress = models.TextField(max_length=200)           #endereco
     tel = models.CharField(max_length=30)                         #telefone
 
     def __str__(self):
-        return self.user_registration
-        
+        return self.username
+
 class Session(models.Model):
     code = models.CharField(max_length=30 ,primary_key=True)
     details = models.CharField(max_length=100)
@@ -25,14 +35,12 @@ class Book(models.Model):
     code = models.CharField(max_length=30, primary_key=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=50)
-    #session_code = models.CharField(max_length=30)
     session_code = models.ForeignKey(Session , to_field="code",on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
 
 class Loan(models.Model):
-    #code = models.ForeignKey(Book, to_field='code')
     code = models.CharField(max_length=30, primary_key=True)
     date_time = models.DateTimeField(default=timezone.now)
     devolution_date = models.DateField()
