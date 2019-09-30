d50c9f55962311433fc3a0b79e96503335cb7700
Lima159 <ialima@fapex.org.br>
2019-09-30 08:17:08 -0300

    Metodo POST funcionando

--- api/app/Http/Controllers/LivroController.php
+++ api/app/Http/Controllers/LivroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Validator;
 
 class LivroController extends Controller
 {
@@ -34,71 +35,26 @@ class LivroController extends Controller
     public function post(Request $request)
     {
         $data = $request->all();
-        //echo '<pre>'; print_r($data); exit();
+        
+        $validacao = \Validator::make($data,[
+            "code" => "required",
+            "title" => "required",
+            "author" => "required",
+            "session_code" => "required",
+        ]);
 
         $client = new Client();
-        $response = $client->requestAsync('POST', 'http://localhost:7000/biblioteca/', [
-            'form_params' => [
-                'code' => '177',
-                'title' => 'Vava',
-                'author' => 'Fagner',
-                'session_code' => '12',
-            ]
-        ]);
-        echo '<pre>'; print_r($response);
-        exit();
-        /*
-        $url = "http://localhost:7000/biblioteca/";
+        
 
-        $options = [
-            'form_params' => [
-                "code" => "1",
-                "title" => "2",
-                "author" => "3",
-                "session_code" => "7",
-            ]
-        ]; 
-
-        $request = $client->post($url, $options);
-        echo '<pre>'; print_r($request); exit();
-        */
-/*
-        $response = $client->request('GET', 'http://localhost:7000/biblioteca/');
-        //$response = $client->request('GET', 'google.com');
-        $statusCode = $response->getStatusCode();
-        $body = $response->getBody()->getContents();
-
-        echo '<pre>'; print_r( $body ); exit;*/
-
-
-/*
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('POST', 'http://localhost:7000/biblioteca/', [
-            'form_params' => [
-                'code' => '1',
-                'title' => '1',
-                'author' => '1',
-                'session_code' => '1',
+        $response = $client->post('http://localhost:7000/biblioteca/', [
+            'json' => [
+                'code' => $data['code'],
+                'title' => $data['title'],
+                'author' => $data['author'],
+                'session_code' => $data['session_code'],
             ]
         ]);
 
-
-        $response = $response->getBody()->getContents();
-        echo '<pre>';
-        print_r($response);
-
-        $client->post(
-        'http://localhost:7000/biblioteca/',
-        array(
-                'form_params' => array(
-                    'code' => "1",
-                    'title' => "1",
-                    'author' => "1",
-                    'session_code' => "1"
-                )
-            )
-        );
-
-        //return view('someview', compact('post'));*/
+        return redirect()->back()->withErrors($validacao)->withInput();
     }
 }
